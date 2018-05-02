26321cbda415b99d7f8d02f92062fe044b1b39b1
yiglas <dsac@hotmail.com>
2018-05-02 08:44:25 -0700

    started core code

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # funcx
 Functional Library for C#
+
+
+todo:
+1. create persistent list & list<t>
+2. create persistent dictionary & dictionary<k, v>
+3. create persistent vector & vector<v>
+4. create persistent set & set<t>
+5. create persistent structuremap & structuremap<k, v>
+7. create persistent queue<t>
+
+funcx.collection.generic
+funcx.collection
+funcx.core
+funcx.string
+funcx.core.async
---
+++ tests/funcx.tests/core.inc.should.cs
@@ -0,0 +1,31 @@
+﻿namespace funcx.tests
+{
+    using NUnit.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using static funcx.core;
+
+    public class core_inc_should
+    {
+        [Test]
+        public void Inc_should_increase_the_number_by_one()
+        {
+            var expected = 1;
+            var actual = inc(0);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Inc_should_return_same_type_as_passed_in()
+        {
+            Assert.IsInstanceOf(typeof(int), inc(1));
+            Assert.IsInstanceOf(typeof(double), inc((double)1));
+            Assert.IsInstanceOf(typeof(float), inc((float)1));
+            Assert.IsInstanceOf(typeof(double), inc((double)1));
+        }
+    }
+}
