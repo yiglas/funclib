8298ab470467433cd5ef23b1aaa0eea56b35992e
Devin Sackett <dsac@hotmail.com>
2018-07-21 07:21:08 -0700

    changed new Disjǃ().Invoke to disjǃ

--- tests/funclib.tests/Components/Core/DisjǃShould.cs
+++ tests/funclib.tests/Components/Core/DisjǃShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -11,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void Disjǃ_should_return_pass_in_set()
         {
             var expected = new Transient().Invoke(new HashSet().Invoke(1, 2, 3));
-            var actual = new Disjǃ().Invoke(expected);
+            var actual = disjǃ(expected);
 
             Assert.IsTrue(expected == actual);
         }
@@ -20,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void Disjǃ_should_return_the_same_object_without_the_give_key()
         {
             var expected = new Transient().Invoke(new HashSet().Invoke(1, 2, 3));
-            var actual = new Disjǃ().Invoke(expected, 3);
+            var actual = disjǃ(expected, 3);
 
             Assert.IsTrue(expected == actual);
         }
@@ -29,7 +30,7 @@ namespace funclib.Tests.Components.Core
         public void Disjǃ_should_return_the_same_object_without_all_the_given_keys()
         {
             var expected = new Transient().Invoke(new HashSet().Invoke(1, 2, 3, 4, 5, 6, 7, 8, 9));
-            var actual = new Disjǃ().Invoke(expected, 2, 4, 6, 8);
+            var actual = disjǃ(expected, 2, 4, 6, 8);
 
             Assert.IsTrue(expected == actual);
         }
