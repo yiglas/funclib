de16d865d4ffefa8ef69102fd7ce5542e07c7a11
Devin Sackett <dsac@hotmail.com>
2018-07-21 07:23:56 -0700

    changed new Dissoc().Invoke to dissoc

--- tests/funclib.tests/Components/Core/DissocShould.cs
+++ tests/funclib.tests/Components/Core/DissocShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -11,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void Dissoc_should_return_passed_in_collection_if_not_passed_object_to_remove()
         {
             var expected = new HashMap().Invoke();
-            var actual = new Dissoc().Invoke(expected);
+            var actual = dissoc(expected);
 
             Assert.IsTrue(expected == actual);
         }
@@ -20,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void Dissoc_should_return_a_new_map_without_the_given_key()
         {
             var expected = new HashMap().Invoke("a", 1, "c", 3);
-            var actual = new Dissoc().Invoke(new HashMap().Invoke("a", 1, "b", 2, "c", 3), "b");
+            var actual = dissoc(new HashMap().Invoke("a", 1, "b", 2, "c", 3), "b");
 
             Assert.AreEqual(expected, actual);
         }
@@ -29,7 +30,7 @@ namespace funclib.Tests.Components.Core
         public void Dissoc_should_return_a_new_map_without_all_the_given_keys()
         {
             var expected = new HashMap().Invoke("a", 1, "c", 3, "e", 5, "g", 7);
-            var actual = new Dissoc().Invoke(new HashMap().Invoke("a", 1, "b", 2, "c", 3, "d", 4, "e", 5, "f", 6, "g", 7), "b", "d", "f");
+            var actual = dissoc(new HashMap().Invoke("a", 1, "b", 2, "c", 3, "d", 4, "e", 5, "f", 6, "g", 7), "b", "d", "f");
 
             Assert.AreEqual(expected, actual);
         }
