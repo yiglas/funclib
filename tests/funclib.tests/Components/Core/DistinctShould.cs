64d157d31621fe6052756f0d16f1ca71e61fe32f
Devin Sackett <dsac@hotmail.com>
2018-07-21 07:27:08 -0700

    changed new Distinct().Invoke to distinct

--- tests/funclib.tests/Components/Core/DistinctShould.cs
+++ tests/funclib.tests/Components/Core/DistinctShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -11,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void Distinct_should_return_lazy_list_of_distinct_values()
         {
             var expected = new funclib.Components.Core.List().Invoke(1, 2, 3, 4, 5);
-            var actual = new ToArray().Invoke(new Distinct().Invoke(new Vector().Invoke(1, 2, 1, 3, 1, 4, 1, 5)));
+            var actual = new ToArray().Invoke(distinct(new Vector().Invoke(1, 2, 1, 3, 1, 4, 1, 5)));
 
             Assert.AreEqual(expected, actual);
         }
