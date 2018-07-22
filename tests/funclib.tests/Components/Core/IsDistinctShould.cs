e56b8359a2b87b621505f10fca59ad3e56f9a741
Devin Sackett <dsac@hotmail.com>
2018-07-22 14:28:15 -0700

    changed new IsDistinct().Invoke to isDistinct

--- tests/funclib.tests/Components/Core/IsDistinctShould.cs
+++ tests/funclib.tests/Components/Core/IsDistinctShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -10,18 +11,18 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsDistinct_should_return_true_if_an_object_is_distinct()
         {
-            Assert.IsTrue((bool)new IsDistinct().Invoke(1));
-            Assert.IsTrue((bool)new IsDistinct().Invoke(1, 2));
-            Assert.IsTrue((bool)new IsDistinct().Invoke(1, 2, 3, 4));
+            Assert.IsTrue((bool)isDistinct(1));
+            Assert.IsTrue((bool)isDistinct(1, 2));
+            Assert.IsTrue((bool)isDistinct(1, 2, 3, 4));
         }
 
         [Test]
         public void IsDistinct_should_return_false_if_an_object_is_not_distinct()
         {
-            Assert.IsFalse((bool)new IsDistinct().Invoke(1, 1));
-            Assert.IsFalse((bool)new IsDistinct().Invoke(1, 1, 2, 3));
-            Assert.IsFalse((bool)new IsDistinct().Invoke(1, 2, 3, 3));
-            Assert.IsFalse((bool)new IsDistinct().Invoke(1, 2, 3, 1));
+            Assert.IsFalse((bool)isDistinct(1, 1));
+            Assert.IsFalse((bool)isDistinct(1, 1, 2, 3));
+            Assert.IsFalse((bool)isDistinct(1, 2, 3, 3));
+            Assert.IsFalse((bool)isDistinct(1, 2, 3, 1));
         }
     }
 }
