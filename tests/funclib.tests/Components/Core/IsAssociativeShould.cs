2dc142e5ec4b64032b14180e7e4262197933a4b3
Devin Sackett <dsac@hotmail.com>
2018-07-22 14:22:21 -0700

    changed new IsAssociative().Invoke to isAssociative

--- tests/funclib.tests/Components/Core/IsAssociativeShould.cs
+++ tests/funclib.tests/Components/Core/IsAssociativeShould.cs
@@ -11,15 +11,15 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsAssociative_should_return_true_if_an_object_that_implements_IAssociative()
         {
-            Assert.IsTrue((bool)new IsAssociative().Invoke(hashMap()));
-            Assert.IsTrue((bool)new IsAssociative().Invoke(arrayMap()));
+            Assert.IsTrue((bool)isAssociative(hashMap()));
+            Assert.IsTrue((bool)isAssociative(arrayMap()));
         }
 
         [Test]
         public void IsAssociative_should_return_false_if_an_object_doesnot_implement_IAssociative()
         {
-            Assert.IsFalse((bool)new IsAssociative().Invoke(1));
-            Assert.IsFalse((bool)new IsAssociative().Invoke(null));
+            Assert.IsFalse((bool)isAssociative(1));
+            Assert.IsFalse((bool)isAssociative(null));
         }
     }
 }
