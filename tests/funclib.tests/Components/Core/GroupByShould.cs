08efc0953e42317de3b5f8d99a5589f4b539ffe4
Devin Sackett <dsac@hotmail.com>
2018-07-22 08:06:14 -0700

    changed new GroupBy().Invoke to groupBy

--- tests/funclib.tests/Components/Core/GroupByShould.cs
+++ tests/funclib.tests/Components/Core/GroupByShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -11,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void GroupBy_should_group_a_given_collection_by_the_function()
         {
             var expected = new HashMap().Invoke(false, new Vector().Invoke(0, 2, 4, 6, 8), true, new Vector().Invoke(1, 3, 5, 7, 9));
-            var actual = new GroupBy().Invoke(new IsOdd(), new Range().Invoke(10));
+            var actual = groupBy(new IsOdd(), new Range().Invoke(10));
 
             Assert.AreEqual(expected, actual);
         }
