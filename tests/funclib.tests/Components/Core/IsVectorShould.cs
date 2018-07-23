15f8aae6c5610e91ec9e38001416e275020e654f
Devin Sackett <dsac@hotmail.com>
2018-07-22 21:50:17 -0700

    changed new IsVector().Invoke to isVector

--- tests/funclib.tests/Components/Core/IsVectorShould.cs
+++ tests/funclib.tests/Components/Core/IsVectorShould.cs
@@ -11,17 +11,17 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsVector_should_return_true_if_an_object_is_false()
         {
-            Assert.IsTrue((bool)new IsVector().Invoke(new Vector().Invoke(1, 2, 3)));
-            Assert.IsTrue((bool)new IsVector().Invoke(new Vec().Invoke(new object[] { 1, 2, 3 })));
-            Assert.IsTrue((bool)new IsVector().Invoke(first(arrayMap(":a", 1, ":b", 2, ":c", 3))));
+            Assert.IsTrue((bool)isVector(new Vector().Invoke(1, 2, 3)));
+            Assert.IsTrue((bool)isVector(new Vec().Invoke(new object[] { 1, 2, 3 })));
+            Assert.IsTrue((bool)isVector(first(arrayMap(":a", 1, ":b", 2, ":c", 3))));
         }
 
         [Test]
         public void IsVector_should_return_false_if_an_object_is_not_false()
         {
-            Assert.IsFalse((bool)new IsVector().Invoke(1));
-            Assert.IsFalse((bool)new IsVector().Invoke(null));
-            Assert.IsFalse((bool)new IsVector().Invoke(hashSet(1, 2, 3)));
+            Assert.IsFalse((bool)isVector(1));
+            Assert.IsFalse((bool)isVector(null));
+            Assert.IsFalse((bool)isVector(hashSet(1, 2, 3)));
         }
     }
 }
