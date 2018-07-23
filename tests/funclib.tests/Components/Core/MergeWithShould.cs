b33c663a31da2252e2bc475efca18ba052680960
Devin Sackett <dsac@hotmail.com>
2018-07-23 09:04:48 -0700

    changed new MergeWith().Invoke to mergeWith

--- tests/funclib.tests/Components/Core/MergeWithShould.cs
+++ tests/funclib.tests/Components/Core/MergeWithShould.cs
@@ -12,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void MergeWith_should_merge_two_maps_with_a_function()
         {
             var expected = hashMap(":a", 10, ":b", 100, ":c", 0);
-            var actual = new MergeWith().Invoke(new Plus(), hashMap(":a", 1, ":b", 2), hashMap(":a", 9, ":b", 98, ":c", 0));
+            var actual = mergeWith(new Plus(), hashMap(":a", 1, ":b", 2), hashMap(":a", 9, ":b", 98, ":c", 0));
 
             Assert.AreEqual(expected, actual);
         }
