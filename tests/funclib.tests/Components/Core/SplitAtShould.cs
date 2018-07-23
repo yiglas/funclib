7a668a848e4aefe6648463b6c094e96be5e907dd
Devin Sackett <dsac@hotmail.com>
2018-07-23 12:21:06 -0700

    changed new SplitAt().Invoke to splitAt

--- tests/funclib.tests/Components/Core/SplitAtShould.cs
+++ tests/funclib.tests/Components/Core/SplitAtShould.cs
@@ -12,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void SplitAt_should_split_a_vector_at_the_given_number()
         {
             var expected = new Vector().Invoke(list(1, 2), list(3, 4, 5));
-            var actual = new SplitAt().Invoke(2, new Vector().Invoke(1, 2, 3, 4, 5));
+            var actual = splitAt(2, new Vector().Invoke(1, 2, 3, 4, 5));
 
             Assert.AreEqual(expected, actual);
         }
@@ -21,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void SplitAt_should_return_empty_list_if_list_greater_then_count_of_coll()
         {
             var expected = new Vector().Invoke(list(1, 2), list());
-            var actual = new SplitAt().Invoke(3, new Vector().Invoke(1, 2));
+            var actual = splitAt(3, new Vector().Invoke(1, 2));
 
             Assert.AreEqual(expected, actual);
         }
