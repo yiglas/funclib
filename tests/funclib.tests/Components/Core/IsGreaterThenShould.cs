8f514cf359418b4ec0885cef62d63de5f66e613d
Devin Sackett <dsac@hotmail.com>
2018-07-22 20:30:30 -0700

    changed new IsGreaterThan().Invoke to isGreaterThan

--- tests/funclib.tests/Components/Core/IsGreaterThenShould.cs
+++ tests/funclib.tests/Components/Core/IsGreaterThenShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -10,31 +11,31 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsGreaterThan_should_return_true_if_passed_one_value()
         {
-            Assert.IsTrue((bool)new IsGreaterThan().Invoke(1));
+            Assert.IsTrue((bool)isGreaterThan(1));
         }
 
         [Test]
         public void IsGreaterThan_should_return_true_if_nums_are_greater()
         {
-            Assert.IsTrue((bool)new IsGreaterThan().Invoke(2, 1));
+            Assert.IsTrue((bool)isGreaterThan(2, 1));
         }
 
         [Test]
         public void IsGreaterThan_should_return_false_if_nums_are_equal()
         {
-            Assert.IsFalse((bool)new IsGreaterThan().Invoke(2, 2));
+            Assert.IsFalse((bool)isGreaterThan(2, 2));
         }
 
         [Test]
         public void IsGreaterThan_should_return_false_if_nums_are_not_greater()
         {
-            Assert.IsFalse((bool)new IsGreaterThan().Invoke(1, 2));
+            Assert.IsFalse((bool)isGreaterThan(1, 2));
         }
 
         [Test]
         public void IsGreaterThan_should_return_true_if_all_nums_are_greater()
         {
-            Assert.IsTrue((bool)new IsGreaterThan().Invoke(2, 1, 0, -1));
+            Assert.IsTrue((bool)isGreaterThan(2, 1, 0, -1));
         }
     }
 }
