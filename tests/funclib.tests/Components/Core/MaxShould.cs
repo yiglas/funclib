abf2a3cbcd19339423ce2a32a5659f6148e932b7
Devin Sackett <dsac@hotmail.com>
2018-07-23 09:03:21 -0700

    changed new Max().Invoke to max

--- tests/funclib.tests/Components/Core/MaxShould.cs
+++ tests/funclib.tests/Components/Core/MaxShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -11,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void Max_should_return_the_value_passed()
         {
             var expected = 100;
-            var actual = new Max().Invoke(100);
+            var actual = max(100);
 
             Assert.AreEqual(expected, actual);
         }
@@ -20,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void Max_should_return_the_max_value_from_passed_in_values()
         {
             var expected = 5;
-            var actual = new Max().Invoke(1, 2, 3, 4, 5);
+            var actual = max(1, 2, 3, 4, 5);
 
             Assert.AreEqual(expected, actual);
         }
