9f9bd0964265e159b8175f30aea44a0f86501528
Devin Sackett <dsac@hotmail.com>
2018-07-22 20:53:52 -0700

    changed new IsNatInt().Invoke to isNatInt

--- tests/funclib.tests/Components/Core/IsNatIntShould.cs
+++ tests/funclib.tests/Components/Core/IsNatIntShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -10,15 +11,15 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsNatInt_should_return_true_if_an_object_is_NonNegative()
         {
-            Assert.IsTrue((bool)new IsNatInt().Invoke(1));
-            Assert.IsTrue((bool)new IsNatInt().Invoke(0));
+            Assert.IsTrue((bool)isNatInt(1));
+            Assert.IsTrue((bool)isNatInt(0));
         }
 
         [Test]
         public void IsNatInt_should_return_false_if_an_object_is_not_NonNegative()
         {
-            Assert.IsFalse((bool)new IsNatInt().Invoke(-1));
-            Assert.IsFalse((bool)new IsNatInt().Invoke(null));
+            Assert.IsFalse((bool)isNatInt(-1));
+            Assert.IsFalse((bool)isNatInt(null));
         }
     }
 }
