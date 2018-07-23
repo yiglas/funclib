b964147cd4ff8f904b3a51a2491f733e71ff2ba9
Devin Sackett <dsac@hotmail.com>
2018-07-23 09:42:48 -0700

    changed new PartitionBy().Invoke to partitionBy

--- tests/funclib.tests/Components/Core/PartitionByShould.cs
+++ tests/funclib.tests/Components/Core/PartitionByShould.cs
@@ -13,7 +13,7 @@ namespace funclib.Tests.Components.Core
         public void PartitionBy_should_when_result_changes()
         {
             var expected = list(list(1, 2), list(3), list(4, 5));
-            var actual = new PartitionBy().Invoke(new Function<object, object>(x => x.Equals(3)), new Vector().Invoke(1, 2, 3, 4, 5));
+            var actual = partitionBy(new Function<object, object>(x => x.Equals(3)), new Vector().Invoke(1, 2, 3, 4, 5));
 
             Assert.AreEqual(expected, actual);
         }
