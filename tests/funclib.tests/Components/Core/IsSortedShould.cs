c367f370df2d4ff430c61ba8693d74ee9bce33ef
Devin Sackett <dsac@hotmail.com>
2018-07-22 21:40:43 -0700

    changed new IsSorted().Invoke to isSorted

--- tests/funclib.tests/Components/Core/IsSortedShould.cs
+++ tests/funclib.tests/Components/Core/IsSortedShould.cs
@@ -2,6 +2,8 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
+
 namespace funclib.Tests.Components.Core
 {
     public class IsSortedShould
@@ -9,13 +11,13 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsSorted_should_return_true_if_implements_sorted()
         {
-            Assert.IsTrue((bool)new IsSorted().Invoke(new SortedSet().Invoke(1, 2, 3)));
+            Assert.IsTrue((bool)isSorted(new SortedSet().Invoke(1, 2, 3)));
         }
 
         [Test]
         public void IsSorted_should_return_false_if_doesnot_implements_sorted()
         {
-            Assert.IsFalse((bool)new IsSorted().Invoke(new Vector().Invoke(1, 2, 3)));
+            Assert.IsFalse((bool)isSorted(new Vector().Invoke(1, 2, 3)));
         }
     }
 }
