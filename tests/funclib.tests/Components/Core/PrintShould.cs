993fe8216f798dbe133a382fc1b33e5bba512bac
Devin Sackett <dsac@hotmail.com>
2018-07-23 10:47:50 -0700

    changed new Print().Invoke to print

--- tests/funclib.tests/Components/Core/PrintShould.cs
+++ tests/funclib.tests/Components/Core/PrintShould.cs
@@ -27,7 +27,7 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void Print_should_write_string()
         {
-            new Print().Invoke("Foo");
+            print("Foo");
             Assert.AreEqual(this._writer.ToString(), "Foo");
         }
 
@@ -35,7 +35,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_double()
         {
             var expected = 10D;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10.0");
         }
 
@@ -43,7 +43,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_float()
         {
             float expected = 10.0F;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10.0");
         }
 
@@ -51,7 +51,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_short()
         {
             var expected = (short)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -59,7 +59,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_int()
         {
             var expected = 10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -67,7 +67,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_long()
         {
             var expected = (long)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -75,7 +75,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_ushort()
         {
             var expected = (ushort)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -83,7 +83,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_uint()
         {
             var expected = (uint)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -91,7 +91,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_ulong()
         {
             var expected = (ulong)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -99,7 +99,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_byte()
         {
             var expected = (byte)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -107,7 +107,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_sbyte()
         {
             var expected = (sbyte)10;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "10");
         }
 
@@ -115,7 +115,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_bool()
         {
             var expected = true;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "true");
         }
 
@@ -123,7 +123,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_char()
         {
             var expected = 'c';
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "\\c");
         }
 
@@ -131,7 +131,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_type()
         {
             var expected = 'c'.GetType();
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "System.Char");
         }
 
@@ -139,7 +139,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_regex()
         {
             var expected = new Regex("\\d+");
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "#\"\\d+\"");
         }
 
@@ -147,7 +147,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_ISeq()
         {
             var expected = new Seq().Invoke(list(1, 2, 3));
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "(1 2 3)");
         }
 
@@ -155,7 +155,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_IVector()
         {
             var expected = new Vector().Invoke(1, 2, 3);
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "[1 2 3]");
         }
 
@@ -163,7 +163,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_IMap()
         {
             var expected = arrayMap(":a", 1, ":b", 2);
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "{\":a\" 1, \":b\" 2}");
         }
 
@@ -171,7 +171,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_ISet()
         {
             var expected = hashSet(1, 2, 3);
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), "#{1 2 3}");
         }
 
@@ -179,7 +179,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_DateTime()
         {
             var expected = DateTime.Now;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), $"#inst \"{expected.ToString("yyyy-MM-ddTHH:mm:ss.fff-00:00")}\"");
         }
 
@@ -187,7 +187,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_DateTimeOffset()
         {
             var expected = DateTimeOffset.Now;
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), $"#inst \"{expected.ToString("yyyy-MM-ddTHH:mm:ss.fffzzzz")}\"");
         }
 
@@ -195,7 +195,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_guid()
         {
             var expected = Guid.NewGuid();
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), $"#uuid \"{expected}\"");
         }
 
@@ -203,7 +203,7 @@ namespace funclib.Tests.Components.Core
         public void Print_should_write_Timespan()
         {
             var expected = TimeSpan.FromDays(1);
-            new Print().Invoke(expected);
+            print(expected);
             Assert.AreEqual(this._writer.ToString(), $"1.00:00:00");
         }
     }
