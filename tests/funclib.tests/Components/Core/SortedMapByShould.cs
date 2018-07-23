a3a92a11047f20495c5f5711d952a9faa4c9029b
Devin Sackett <dsac@hotmail.com>
2018-07-23 12:19:34 -0700

    changed new SortedMapBy().Invoke to sortedMapBy

--- tests/funclib.tests/Components/Core/SortedMapByShould.cs
+++ tests/funclib.tests/Components/Core/SortedMapByShould.cs
@@ -11,7 +11,7 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void SortedMapBy_should_create_new_map_sorted_by_the_function()
         {
-            var actual = new SortedMapBy().Invoke(new IsGreaterThan(), 1, "a", 2, "b", 3, "c");
+            var actual = sortedMapBy(new IsGreaterThan(), 1, "a", 2, "b", 3, "c");
 
             Assert.AreEqual(new Vector().Invoke(3, "c"), first(actual));
             Assert.AreEqual(new Vector().Invoke(2, "b"), second(actual));
