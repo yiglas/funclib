b1ae4e27720c815887fd01cf655569c1eca7be10
Devin Sackett <dsac@hotmail.com>
2018-07-22 20:50:44 -0700

    changed new IsList().Invoke to isList

--- tests/funclib.tests/Components/Core/IsListShould.cs
+++ tests/funclib.tests/Components/Core/IsListShould.cs
@@ -2,6 +2,7 @@
 using NUnit.Framework;
 using System;
 using System.Text;
+using static funclib.Core;
 
 namespace funclib.Tests.Components.Core
 {
@@ -10,14 +11,14 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsList_should_return_true_if_an_object_is_IList()
         {
-            Assert.IsTrue((bool)new IsList().Invoke(new funclib.Components.Core.List().Invoke()));
+            Assert.IsTrue((bool)isList(new funclib.Components.Core.List().Invoke()));
         }
 
         [Test]
         public void IsList_should_return_false_if_an_object_is_not_IList()
         {
-            Assert.IsFalse((bool)new IsList().Invoke(new Vector().Invoke()));
-            Assert.IsFalse((bool)new IsList().Invoke(null));
+            Assert.IsFalse((bool)isList(new Vector().Invoke()));
+            Assert.IsFalse((bool)isList(null));
         }
     }
 }
