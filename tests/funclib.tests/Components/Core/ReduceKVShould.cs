3368f9159af967aac24b2f8dd9cf03e184fb68c5
Devin Sackett <dsac@hotmail.com>
2018-07-23 11:04:00 -0700

    changed new ReduceKV().Invoke to reduceKV

--- tests/funclib.tests/Components/Core/ReduceKVShould.cs
+++ tests/funclib.tests/Components/Core/ReduceKVShould.cs
@@ -12,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void ReduceKV_should_be_able_to_swap_key_and_values_in_a_map()
         {
             var expected = arrayMap(1, ":a", 2, ":b", 3, ":c");
-            var actual = new ReduceKV().Invoke(
+            var actual = reduceKV(
                 new Function<object, object, object, object>((_1, _2, _3) => assoc(_1, _3, _2)),
                 arrayMap(),
                 arrayMap(":a", 1, ":b", 2, ":c", 3));
