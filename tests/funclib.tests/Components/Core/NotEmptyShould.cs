fb8bee2383cc9e116eaf458c03444f55a375ac87
Devin Sackett <dsac@hotmail.com>
2018-07-23 09:26:51 -0700

    changed new NotEmpty().Invoke to notEmpty

--- tests/funclib.tests/Components/Core/NotEmptyShould.cs
+++ tests/funclib.tests/Components/Core/NotEmptyShould.cs
@@ -11,17 +11,17 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void NotEmpty_should_return_null_for_empty_collections()
         {
-            Assert.IsNull(new NotEmpty().Invoke(new Vector().Invoke()));
-            Assert.IsNull(new NotEmpty().Invoke(list()));
-            Assert.IsNull(new NotEmpty().Invoke(hashMap()));
-            Assert.IsNull(new NotEmpty().Invoke(null));
+            Assert.IsNull(notEmpty(new Vector().Invoke()));
+            Assert.IsNull(notEmpty(list()));
+            Assert.IsNull(notEmpty(hashMap()));
+            Assert.IsNull(notEmpty(null));
         }
 
         [Test]
         public void NotEmpty_should_return_the_object_if_collection_is_not_empty()
         {
             var expected = new Vector().Invoke(1, 2, 3);
-            var actual = new NotEmpty().Invoke(expected);
+            var actual = notEmpty(expected);
 
             Assert.IsTrue(expected == actual);
         }
