0bb5a3681151f1c459272b1895e2961eff226e03
Devin Sackett <dsac@hotmail.com>
2018-07-23 11:35:23 -0700

    changed new SelectKeys().Invoke to selectKeys

--- tests/funclib.tests/Components/Core/SelectKeysShould.cs
+++ tests/funclib.tests/Components/Core/SelectKeysShould.cs
@@ -12,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void SelectKeys_should_pull_keys_that_exists_from_a_map()
         {
             var expected = arrayMap(":a", 1);
-            var actual = new SelectKeys().Invoke(arrayMap(":a", 1, ":b", 2), new Vector().Invoke(":a"));
+            var actual = selectKeys(arrayMap(":a", 1, ":b", 2), new Vector().Invoke(":a"));
 
             Assert.AreEqual(expected, actual);
         }
@@ -21,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void SelectKeys_should_pull_those_keys_that_exists_from_a_map()
         {
             var expected = arrayMap(":a", 1);
-            var actual = new SelectKeys().Invoke(arrayMap(":a", 1, ":b", 2), new Vector().Invoke(":a", ":c"));
+            var actual = selectKeys(arrayMap(":a", 1, ":b", 2), new Vector().Invoke(":a", ":c"));
 
             Assert.AreEqual(expected, actual);
         }
@@ -29,7 +29,7 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void SelectedKeys_should_return_a_hashmap()
         {
-            var actual = new SelectKeys().Invoke(arrayMap(":a", 1, ":b", 2), new Vector().Invoke(":a", ":c"));
+            var actual = selectKeys(arrayMap(":a", 1, ":b", 2), new Vector().Invoke(":a", ":c"));
 
             Assert.IsInstanceOf<funclib.Collections.HashMap>(actual);
         }
@@ -38,7 +38,7 @@ namespace funclib.Tests.Components.Core
         public void SelectedKeys_should_work_with_passing_vectors_as_well()
         {
             var expected = arrayMap(0, 1, 2, 3);
-            var actual = new SelectKeys().Invoke(new Vector().Invoke(1, 2, 3), new Vector().Invoke(0, 0, 2));
+            var actual = selectKeys(new Vector().Invoke(1, 2, 3), new Vector().Invoke(0, 0, 2));
 
             Assert.AreEqual(expected, actual);
         }
