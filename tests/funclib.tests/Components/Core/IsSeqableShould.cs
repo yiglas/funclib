b1c55ff1dc7215b1aa710920a6e4728178bf46e0
Devin Sackett <dsac@hotmail.com>
2018-07-22 21:35:15 -0700

    changed new IsSeqable().Invoke to isSeqable

--- tests/funclib.tests/Components/Core/IsSeqableShould.cs
+++ tests/funclib.tests/Components/Core/IsSeqableShould.cs
@@ -11,11 +11,11 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void IsSeqable_should_return_true_if_object_is_seqable()
         {
-            Assert.IsTrue((bool)new IsSeqable().Invoke(null));
-            Assert.IsTrue((bool)new IsSeqable().Invoke(""));
-            Assert.IsTrue((bool)new IsSeqable().Invoke(new System.Collections.Generic.List<string>()));
-            Assert.IsTrue((bool)new IsSeqable().Invoke(new Vector().Invoke()));
-            Assert.IsTrue((bool)new IsSeqable().Invoke(new LazySeq()));
+            Assert.IsTrue((bool)isSeqable(null));
+            Assert.IsTrue((bool)isSeqable(""));
+            Assert.IsTrue((bool)isSeqable(new System.Collections.Generic.List<string>()));
+            Assert.IsTrue((bool)isSeqable(new Vector().Invoke()));
+            Assert.IsTrue((bool)isSeqable(new LazySeq()));
         }
 
         [Test]
