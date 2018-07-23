514e20cbb956515f44d0141eb960f0c4fb990b14
Devin Sackett <dsac@hotmail.com>
2018-07-23 12:31:30 -0700

    changed new TakeLast().Invoke to takeLast

--- tests/funclib.tests/Components/Core/TakeLastShould.cs
+++ tests/funclib.tests/Components/Core/TakeLastShould.cs
@@ -12,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void TakeLast_should_return_last_number()
         {
             var expected = list(3, 4);
-            var actual = new TakeLast().Invoke(2, new Vector().Invoke(1, 2, 3, 4));
+            var actual = takeLast(2, new Vector().Invoke(1, 2, 3, 4));
 
             Assert.AreEqual(expected, actual);
         }
@@ -21,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void TakeLast_should_return_complete_list_if_number_is_greater_than_list()
         {
             var expected = list(4);
-            var actual = new TakeLast().Invoke(2, new Vector().Invoke(4));
+            var actual = takeLast(2, new Vector().Invoke(4));
 
             Assert.AreEqual(expected, actual);
         }
@@ -29,19 +29,19 @@ namespace funclib.Tests.Components.Core
         [Test]
         public void TakeLast_should_return_null_with_empty_coll()
         {
-            Assert.IsNull(new TakeLast().Invoke(2, new Vector().Invoke()));
+            Assert.IsNull(takeLast(2, new Vector().Invoke()));
         }
 
         [Test]
         public void TakeLast_should_return_null_with_null_coll()
         {
-            Assert.IsNull(new TakeLast().Invoke(2, null));
+            Assert.IsNull(takeLast(2, null));
         }
 
         [Test]
         public void TakeLast_should_return_null_with_neg_number()
         {
-            var actual = new TakeLast().Invoke(-1, new Vector().Invoke(4));
+            var actual = takeLast(-1, new Vector().Invoke(4));
         }
     }
 }
