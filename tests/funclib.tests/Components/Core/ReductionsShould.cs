617774546d3ebdfe2f3a238e3929d16996e95fd6
Devin Sackett <dsac@hotmail.com>
2018-07-23 11:06:11 -0700

    changed new Reductions().Invoke to reductions

--- tests/funclib.tests/Components/Core/ReductionsShould.cs
+++ tests/funclib.tests/Components/Core/ReductionsShould.cs
@@ -12,7 +12,7 @@ namespace funclib.Tests.Components.Core
         public void Reductions_should_sum_each_item_with_its_prediceser()
         {
             var expected = list(1, 2, 3, 4);
-            var actual = new Reductions().Invoke(new Plus(), new Vector().Invoke(1, 1, 1, 1));
+            var actual = reductions(new Plus(), new Vector().Invoke(1, 1, 1, 1));
 
             Assert.AreEqual(expected, actual);
         }
@@ -21,7 +21,7 @@ namespace funclib.Tests.Components.Core
         public void Reductions_should_conj_vectors()
         {
             var expected = new Vector().Invoke(new Vector().Invoke(), new Vector().Invoke(1), new Vector().Invoke(1, 2), new Vector().Invoke(1, 2, 3));
-            var actual = new Reductions().Invoke(new Conj(), new Vector().Invoke(), list(1, 2, 3));
+            var actual = reductions(new Conj(), new Vector().Invoke(), list(1, 2, 3));
 
             Assert.AreEqual(expected, actual);
         }
