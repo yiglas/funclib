5e6ef50ae58a6bae53fe4852f15be77690a9a9b3
yiglas <dsac@hotmail.com>
2019-05-08 15:40:54 -1000

    Testing with Generics

--- src/funclib/Generic/Core.cs
+++ src/funclib/Generic/Core.cs
@@ -1,13 +1,65 @@
-﻿using funclib.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq.Expressions;
+using funclib.Collections.Generic;
 using funclib.Components.Core.Generic;
 
 namespace funclib.Generic
 {
     public static class Core
     {
-        public static bool And() => new And().Invoke();
-        public static T And<T>(T x) => new And<T>().Invoke(x);
-        public static T And<T>(T x, params T[] next) => new And<T>().Invoke(x, next);
+
+        public static string Tess(Expression ex, string test)
+        {
+            return "test";
+        }
+
+        public static string Testing(string s)
+        {
+            return "map";
+        }
+
+        public static string Tte()
+        {
+            return Tess(Testing, "hello");
+        }
+
+
+        // map(Plus, new [] {1, 2, 3})
+        public static string Run<T>(LambdaExpression ex, IEnumerable<T> ints)
+        {
+
+        }
+
+        public static int Inc(int x) => x + 1;
+
+        public static void Testing()
+        {
+            var x = Run(Inc, new[] { 1, 2, 3 });
+        }
+
+
+        public static IFunction and() => new And();
+        public static IFunction and<T>() => new And<T>();
+        // public static IFunction<T, T> and<T>() => new And<T>();
+        // public static IFunctionParams<T, T, T> andP<T>() => new And<T>();
+
+
+        public static bool And() => (and() as IFunction<bool>).Invoke();
+        public static T And<T>(T x) => (and<T>() as IFunction<T, T>).Invoke(x);
+        public static T And<T>(T x, params T[] next) => (and<T>() as IFunctionParams<T, T, T>).Invoke(x, next);
+
+        public static void Test<T>(System.Func<T, T> a, T b)
+        {
+            var x = a(b);
+        }
+
+        public static void Test2()
+        {
+            int b = 0;
+
+            Test(And, b);
+        }
+
 
         public static bool Falsy<T>(T source) => new Falsy<T>().Invoke(source);
         public static bool Truthy<T>(T source) => new Truthy<T>().Invoke(source);
@@ -37,5 +89,7 @@ namespace funclib.Generic
         public static int Count<T>(T coll) => new Count<T>().Invoke(coll);
 
         public static bool Falsy<T>(T source) => new Falsy<T>().Invoke(source);
+
+        public static IFunction Falsy => new Falsy<T>
     }
 }
